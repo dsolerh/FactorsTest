9ae9a75362b35c3e7af965adf7f11a1b717bf703
dsolerh <dsolerh.cinter95@gmail.com>
2021-10-15 20:17:00 -0400

    test for IsPrime

--- FactorsTest.cs
+++ FactorsTest.cs
@@ -1,14 +1,30 @@
 using System;
 using Xunit;
+using FactorsLib;
 
 namespace FactorsTest
 {
     public class FactorsTest
     {
         [Fact]
-        public void Test1()
+        public void TestIsPrime()
         {
+            // arrange
+            int[] x = { 1, 2, 10, 37, 81 };
+            bool[] expected = { true, true, false, true, false };
 
+            // act
+            bool[] result = new bool[5];
+            for (int i = 0; i < 5; i++)
+            {
+                result[i] = Factors.IsPrime(x[i]);
+            }
+
+            // assert
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.Equal(expected[i], result[i]);
+            }
         }
     }
 }
